a97101334fc569dff0ba16839bfcad5b3b596d71
Adachi Sakura <805983409@qq.com>
2020-04-05 02:52:52 +0800

    merge config map

--- k8s-iperf3.sh
+++ k8s-iperf3.sh
@@ -6,15 +6,15 @@ cd $(dirname $0)
 csv="/tmp/result.csv"
 
 
-
 scripts/setup.sh
 scripts/run.sh ${csv}
 scripts/cleanup.sh
 
 config="gok8s-cfg"
-kubectl create configmap ${config} --from-file=$(basename ${csv})
 
-prom-config="prom-config"
+#prom-config="prom-config"
 host=$(kubectl get svc --all-namespaces | awk '$2=="prometheus"{print $4}')
 port=$(kubectl get svc --all-namespaces | awk '$2=="prometheus"{print $6}' | cut -d '/' -f1|cut -d ':' -f1)
-kubectl create configmap ${prom-config} --from-literal=PROMETHEUS_SERVICE_HOST=${host} --from-literal=PROMETHEUS_SERVICE_PORT=${port}
+kubectl create configmap ${config} --from-literal=PROMETHEUS_SERVICE_HOST=${host} --from-literal=PROMETHEUS_SERVICE_PORT=${port} --from-file=$(basename ${csv})
+
+#kubectl get nodes | tail -2 | awk '$3!="master"{system("kubectl label node "$1" container-allocation/name="$1)}'
