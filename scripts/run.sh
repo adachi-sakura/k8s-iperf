356a5319f81dcbd39163eeb72726875a76a42133
Adachi Sakura <805983409@qq.com>
2020-05-14 15:41:16 +0800

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,11 +3,11 @@ set -eu
 
 orchestrator=$(kubectl get po -l app=orchestrator -o name | head -1 | cut -d'/' -f2)
 csv=$1
-while [test -z "$(kubectl exec po ${orchestrator} -- cat ${csv} 2>/dev/null | grep 'END CSV DATA')]; do
+while [ -z "$(kubectl exec ${orchestrator} -- cat ${csv} 2>/dev/null | grep 'END CSV DATA')" ]; do
 	echo "Waiting for network test complete..."
 	sleep 10
 done
 
 echo "test done, writing result into file..."
-echo $(kubectl exec po ${orchestrator -- cat ${csv}) > $(basename ${csv})
+echo "$(kubectl exec ${orchestrator} -- cat ${csv})" > $(basename ${csv})
 echo
