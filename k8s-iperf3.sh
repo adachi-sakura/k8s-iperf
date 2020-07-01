89e53db1d5727a009c5faaa25b00c5dd825c0471
Adachi Sakura <805983409@qq.com>
2020-07-01 18:44:48 +0800

    fix(configmap): create config map in corresponding namespace

--- k8s-iperf3.sh
+++ k8s-iperf3.sh
@@ -15,6 +15,6 @@ config="gok8s-cfg"
 #prom-config="prom-config"
 host=$(kubectl get svc --all-namespaces | awk '$2=="prometheus"{print $4}')
 port=$(kubectl get svc --all-namespaces | awk '$2=="prometheus"{print $6}' | cut -d '/' -f1|cut -d ':' -f1)
-kubectl create configmap ${config} --from-literal=PROMETHEUS_SERVICE_HOST=${host} --from-literal=PROMETHEUS_SERVICE_PORT=${port} --from-file=$(basename ${csv})
+kubectl create -n ${1:-allocation} configmap ${config} --from-literal=PROMETHEUS_SERVICE_HOST=${host} --from-literal=PROMETHEUS_SERVICE_PORT=${port} --from-file=$(basename ${csv})
 
 #kubectl get nodes | tail -2 | awk '$3!="master"{system("kubectl label node "$1" container-allocation/name="$1)}'
